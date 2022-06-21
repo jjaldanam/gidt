560ad252e407c44a6bff9e7abb31d5c585df3965
jjaldanam <jjaldana@gmail.com>
2022-06-20 20:54:30 -0500

    Organizado todos los link - inicio Acordeon Estudintes - Preguntas frecuentes

--- responsivo/js/script.js
+++ responsivo/js/script.js
@@ -1,11 +1,13 @@
-$(document).ready(function (){
-    var altura = $("nav").offset().top;
 
-    $(window).on("scroll",function () {
-        if( $(window).scrollTop() > altura ){
-            $("nav").addClass("menu-fixed");
-        }else{
-            $("nav").removeClass("menu-fixed");
-        }
-    });
-});
+// BARRA DE NAVEGACION FIJA AL HACER SCROLL
+// $(document).ready(function (){
+//     var altura = $("nav").offset().top;
+//
+//     $(window).on("scroll",function () {
+//         if( $(window).scrollTop() > altura ){
+//             $("nav").addClass("menu-fixed");
+//         }else{
+//             $("nav").removeClass("menu-fixed");
+//         }
+//     });
+// });
